f333dc9e48734a3893715e4cc959fb4bc1a629c3
adriano-ramoss <adrianoramos2401@gmail.com>
2023-05-25 19:34:37 -0300

    Novos Fragments

--- app/src/main/java/br/com/etecia/myapp/GatoFragment.java
+++ app/src/main/java/br/com/etecia/myapp/GatoFragment.java
@@ -1,19 +1,18 @@
 package br.com.etecia.myapp;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class CachorroFragment extends Fragment {
+public class GatoFragment extends Fragment {
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cachorro, container, false);
+        return inflater.inflate(R.layout.fragment_gato, container, false);
     }
 }
--- app/src/main/java/br/com/etecia/myapp/PassaroFragment.java
+++ app/src/main/java/br/com/etecia/myapp/PassaroFragment.java
@@ -8,12 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class CachorroFragment extends Fragment {
+public class PassaroFragment extends Fragment {
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cachorro, container, false);
+        return inflater.inflate(R.layout.fragment_passaro, container, false);
     }
 }
