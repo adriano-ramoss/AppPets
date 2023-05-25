6dbee23285e131b2ec44cafc75a9106f0877ace9
adriano-ramoss <adrianoramos2401@gmail.com>
2023-05-25 20:05:15 -0300

    Orientação a Objeto

--- app/src/main/java/br/com/etecia/myapp/CachorroFragment.java
+++ app/src/main/java/br/com/etecia/myapp/CachorroFragment.java
@@ -10,10 +10,12 @@ import android.view.ViewGroup;
 
 public class CachorroFragment extends Fragment {
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cachorro, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_cachorro, container, false);
+        return view;
     }
 }
