c38bed005069d23c16eccb40973cce3910e2f9f8
adriano-ramoss <adrianoramos2401@gmail.com>
2023-05-25 21:20:36 -0300

    OOP

---
+++ app/src/main/java/br/com/etecia/myapp/MyAdapterViewPager.java
@@ -0,0 +1,26 @@
+package br.com.etecia.myapp;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class MyAdapterViewPager extends FragmentStatePagerAdapter {
+
+    public MyAdapterViewPager(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
